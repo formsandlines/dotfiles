c4825fb85cfea86cf1f549536f2a41db1653e1b3
Peter Hofmann <peter.hofmann@formsandlines.eu>
2023-11-16 23:31:29 +0100

    .emacs.d: meow changes/additions, custom commands, slash-prefix fix
    Added hooks to disable slash-prefix from clj-refactor in normal and paren state.
    
    Refactored meow state bindings and changed/added some more.
    
    Wrote a more general `eval-buffer` command for meow.
    
    Wrote a `wrap-with-char` command for a more general surround function.
    
    'ph/meow-insert-exit' is wip, should return to the state that called the insert
    state in the first place, but I don’t know how to do that yet.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,6 +5,14 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("eab4154bd3b8ec5cf90a5a2a14545ba3dc5dd7cf99fcdce013d10475c08ea56d" "7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" default))
+ '(ignored-local-variable-values
+   '((eval font-lock-add-keywords nil
+	   `((,(concat "("
+		       (regexp-opt
+			'("sp-do-move-op" "sp-do-move-cl" "sp-do-put-op" "sp-do-put-cl" "sp-do-del-op" "sp-do-del-cl")
+			t)
+		       "\\_>")
+	      1 'font-lock-variable-name-face)))))
  '(package-selected-packages
    '(lispyville evil-commentary magit clj-refactor diminish company flycheck-clj-kondo flycheck lua-mode haskell-mode rainbow-mode org-appear evil-surround gruvbox-theme evil)))
 (custom-set-faces
